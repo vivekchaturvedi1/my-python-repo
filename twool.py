a56e1078c32bf079537519bfbc02d47c5df73966
Vivek Chaturvedi <vivek.chaturvedi@vertisinfotech.com>
2014-12-03 11:36:42 +0530

    Made few changes to follow python coding standards.

--- twool.py
+++ twool.py
@@ -1,9 +1,11 @@
+#stdlib
 import argparse
 
+#3rd party lib
+import tweepy
 import yaml
 
-import tweepy
-
+#with context manager
 with open("config/user_data.yml", 'r') as ymlfile:
   cfg = yaml.load(ymlfile)
 
@@ -12,15 +14,15 @@ auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
 api = tweepy.API(auth)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-u', '--user', dest='user_name',
+parser.add_argument('-u', '--user', dest='userName',
 	help='The user_name of any authenticated tweeter user')
 parser.add_argument('-c', '--count', dest='count', type=int,
 	help='The no of last tweets on the user account', default=5)
 
 namespace=parser.parse_args()
 
-if (namespace.user_name):
-	timeline = api.user_timeline(screen_name=namespace.user_name,
+if (namespace.userName):
+	timeline = api.user_timeline(screen_name=namespace.userName,
 	count=namespace.count)
 	for tweet in timeline:
 		print (tweet.text)
